b738f4130dc75fc673725d0f1a15b2f064d08b86
tyoung1738 <tomyoung1738@gmail.com>
2024-03-14 09:24:35 +0000

    Added delete comment, with req'd changes

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -12,13 +12,13 @@ export default function Comments({comments, setComments, article_id}){
                 const { comments } = data
                 setComments(comments)
             })
-    }, [article_id])
+    }, [article_id, comments])
     
     return (<div>
             <h3>Comments</h3>
             <ul>
                 {comments.map((comment)=>{
-                return <CommentCard comment={comment} key={comment.comment_id}/>})
+                return <CommentCard comment={comment} key={comment.comment_id} comments={comments} setComments={setComments}/>})
                 }
             </ul>
         </div>)
---
+++ src/Components/Deleting.jsx
@@ -0,0 +1,8 @@
+import { FaRegTrashAlt } from "react-icons/fa";
+
+export default function Deleting(){
+    return (<>
+    <h2>We're deleting your comment...</h2>
+    <FaRegTrashAlt id="deleting-icon" />
+    </>)
+}
