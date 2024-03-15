f2a7fafa008b61b79350618358cd3df955c957db
tyoung1738 <tomyoung1738@gmail.com>
2024-03-15 16:41:35 +0000

    with icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom'
 export default function Header (){
     return (<div className='header'>
-       <Link to={'/'}><h1>Send News</h1></Link>
+        <Link to={'/'}>
+            <h1 id='title'>Send</h1>
+            <img id='home-icon' src="../../newsIcon.png"></img>
+        </Link>
     </div>)
 }
